6bfddc8971cbebc805dda63e7616be5f3cdbc5d8
Jan Kaifer <jan@kaifer.cz>
2023-04-11 17:46:55 +0200

    demo with raw otel packages

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,33 @@
-import { registerOTel } from '@vercel/otel'
-
 export function register() {
-  registerOTel('next-app')
+  console.log("register", process.env.NEXT_RUNTIME);
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const requireSoSlow = require("require-so-slow");
+
+    const { Resource } = require("@opentelemetry/resources");
+    const {
+      SemanticResourceAttributes,
+    } = require("@opentelemetry/semantic-conventions");
+    const {
+      NodeTracerProvider,
+      SimpleSpanProcessor,
+    } = require("@opentelemetry/sdk-trace-node");
+    const {
+      OTLPTraceExporter,
+    } = require("@opentelemetry/exporter-trace-otlp-http");
+
+    const provider = new NodeTracerProvider({
+      resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
+      }),
+    });
+
+    provider.register();
+
+    provider.addSpanProcessor(
+      new SimpleSpanProcessor(new OTLPTraceExporter({}))
+    );
+
+    requireSoSlow.write("require-trace.trace");
+    console.log("written require-trace.trace");
+  }
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const ImportTimingPlugin = require("./import-timing-plugin");
+
 module.exports = {
   experimental: {
     instrumentationHook: true,
     appDir: true,
   },
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new ImportTimingPlugin());
+    }
+    return config;
+  },
+};
