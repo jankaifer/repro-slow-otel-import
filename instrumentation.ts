8d9336c4fdb337293b3bee3cadb39e48c5022fe9
Jan Kaifer <jan@kaifer.cz>
2023-04-11 17:48:51 +0200

    switch back to vercel/otel

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,31 +1,35 @@
+import { registerOTel } from "@vercel/otel";
+
 export function register() {
   console.log("register", process.env.NEXT_RUNTIME);
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const requireSoSlow = require("require-so-slow");
 
-    const { Resource } = require("@opentelemetry/resources");
-    const {
-      SemanticResourceAttributes,
-    } = require("@opentelemetry/semantic-conventions");
-    const {
-      NodeTracerProvider,
-      SimpleSpanProcessor,
-    } = require("@opentelemetry/sdk-trace-node");
-    const {
-      OTLPTraceExporter,
-    } = require("@opentelemetry/exporter-trace-otlp-http");
+    // const { Resource } = require("@opentelemetry/resources");
+    // const {
+    //   SemanticResourceAttributes,
+    // } = require("@opentelemetry/semantic-conventions");
+    // const {
+    //   NodeTracerProvider,
+    //   SimpleSpanProcessor,
+    // } = require("@opentelemetry/sdk-trace-node");
+    // const {
+    //   OTLPTraceExporter,
+    // } = require("@opentelemetry/exporter-trace-otlp-http");
 
-    const provider = new NodeTracerProvider({
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
-      }),
-    });
+    // const provider = new NodeTracerProvider({
+    //   resource: new Resource({
+    //     [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
+    //   }),
+    // });
 
-    provider.register();
+    // provider.register();
 
-    provider.addSpanProcessor(
-      new SimpleSpanProcessor(new OTLPTraceExporter({}))
-    );
+    // provider.addSpanProcessor(
+    //   new SimpleSpanProcessor(new OTLPTraceExporter({}))
+    // );
+
+    registerOTel("next-app");
 
     requireSoSlow.write("require-trace.trace");
     console.log("written require-trace.trace");
